cfe0bdba8273dfdbbbc41f306ed804c5e59e2f2c
thiagodnt <thiagodnt@hotmail.com>
2026-04-20 19:13:21 -0300

    feat: layout inicial da pagina home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { Roboto } from 'next/font/google';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const roboto = Roboto({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<main className={roboto.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
