9e8bb46ce3c4dd5a16727e99ecb3339f236736c4
thiagodnt <thiagodnt@hotmail.com>
2026-04-26 00:00:53 -0300

    feat: login google

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link';
 import styles from './header.module.css';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { Loader } from '../Loader';
 
 export default function Header() {
+	const { data: session, status } = useSession();
+
 	return (
 		<header className={styles.header}>
 			<section className={styles.content}>
@@ -12,11 +16,23 @@ export default function Header() {
 							Tasks
 						</h1>
 					</Link>
-					<Link href="/dashboard" className={styles.link}>
-						Meu painel
-					</Link>
+					{session?.user && (
+						<Link href="/dashboard" className={styles.link}>
+							Meu painel
+						</Link>
+					)}
 				</nav>
-				<button className={styles.loginButton}>Acessar</button>
+				{status === 'loading' ? (
+					<Loader />
+				) : session ? (
+					<button className={styles.loginButton} onClick={() => signOut()}>
+						Olá, {session?.user?.name}
+					</button>
+				) : (
+					<button className={styles.loginButton} onClick={() => signIn('google')}>
+						Acessar
+					</button>
+				)}
 			</section>
 		</header>
 	);
---
+++ components/Loader/index.tsx
@@ -0,0 +1,20 @@
+import styles from './loader.module.css';
+
+interface SpinnerProps {
+	size?: number;
+	color?: string;
+}
+
+export function Loader({ size = 24, color = '#fafafa' }: SpinnerProps) {
+	return (
+		<div
+			style={{
+				width: size,
+				height: size,
+				border: `${size * 0.12}px solid rgba(0,0,0,0.1)`,
+				borderTop: `${size * 0.12}px solid ${color}`,
+			}}
+			className={styles.loader}
+		/>
+	);
+}
