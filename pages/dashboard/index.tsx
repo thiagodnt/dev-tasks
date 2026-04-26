95148c71cdd57285ef926e26b7a82a3ea431e98a
thiagodnt <thiagodnt@hotmail.com>
2026-04-26 00:27:36 -0300

    feat: add GetServerSideProps para validar sessão no server side

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next';
 import styles from './dashboard.module.css';
 import Head from 'next/head';
+import { getSession } from 'next-auth/react';
 
 export default function Dashboard() {
 	return (
@@ -11,3 +13,20 @@ export default function Dashboard() {
 		</div>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const session = await getSession({ req });
+
+	if (!session?.user) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+};
